3f0d63517df90c8fb0356d302eca9818a700d6d5
Milo Sobral <35710239+milo-sobral@users.noreply.github.com>
2022-03-09 17:35:15 -0500

    changed type to int8

--- test_model.py
+++ test_model.py
@@ -15,7 +15,7 @@ def test_model(filename, time):
 
     # Test the model on random input data.
     input_shape = input_details[0]['shape']
-    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+    input_data = np.array(np.random.random_sample(input_shape), dtype=np.int8)
     interpreter.set_tensor(input_details[0]['index'], input_data)
 
     start_time = time.time()
